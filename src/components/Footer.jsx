d041c6f25a71edf22ee4b253e5274f769e0e9c24
albinamels <albina.mels@gmail.com>
2024-02-06 12:06:39 -0800

    fotter section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="pt-4 pb-12 flex align-element">
+      <h4 className="capitalize text-xl text-slate-700 tracking-wide">
+        Albina Mels &copy; {new Date().getFullYear()}
+      </h4>
+    </div>
+  );
+};
+
+export default Footer;
